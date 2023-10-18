4031a64f00b539b876a64f446237d23e5f8f7358
Will Woodford <will62185@gmail.com>
2023-10-18 22:07:19 +0000

    #49 test our terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
   # }
 
   required_providers {
-    # aws = {
-    #   source  = "hashicorp/aws"
-    #   version = "5.17.0"
-    # }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
     terratowns = {
       source  = "local.providers/local/terratowns"
       version = "1.0.0"
@@ -19,36 +19,37 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   # Configuration options
-# }
-
-provider "terratowns" {
-  endpoint  = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+provider "aws" {
+  # Configuration options
 }
 
-# module "terrahouse_aws" {
-#   source              = "./modules/terrahouse_aws"
-#   user_uuid           = var.user_uuid
-#   bucket_name         = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version     = var.content_version
-#   assets_path         = var.assets_path
-# }
+provider "terratowns" {
+  # endpoint  = "http://localhost:4567/api"
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token     = var.terratowns_access_token
+}
+
+module "terrahouse_aws" {
+  source              = "./modules/terrahouse_aws"
+  user_uuid           = var.teacherseat_user_uuid
+  # bucket_name         = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version     = var.content_version
+  assets_path         = var.assets_path
+}
 
 resource "terratowns_home" "home" {
-  name = "Intro to World of Warcraft in 2023!"
+  name        = "Intro to World of Warcraft in 2023!"
   description = <<Description
 World of Warcraft changed the landscape of MMORPGS Forever.
 People of all ages played it, and obsesed over it.
 Blizzard, the company that created the game, recently re-released it,
 calling it World of Warcraft Classic.
 Description
-  # domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "abc3dgrz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  # domain_name = "abc3dgrz.cloudfront.net"
+  town            = "missingo"
   content_version = 1
 }
--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -1,7 +1,8 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "website_bucket" {
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
+  # we want to assign a random bucket name
+  # bucket = var.bucket_name
 
   tags = {
     UserUuid = var.user_uuid
